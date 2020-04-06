f051b044410f5c59fee65e82c7ccd59f83bd6c72
Pedro Trindade <pedrotrindade@Pedros-MacBook-Pro.local>
2020-04-06 15:02:00 -0500

    Implement get post by id

--- functions/src/routes/posts.ts
+++ functions/src/routes/posts.ts
@@ -1,14 +1,14 @@
 import * as admin from "firebase-admin";
 
-import PostSummary from "../models/post_summary";
 import Translation from "../models/translation";
+import Post from "../models/post";
 
 const firestore = admin.firestore();
 
 const router = require("express").Router();
 
-router.get("/summary", async (req: any, res: any) => {
-  const language = req.body.language as string;
+router.get("/summary/:language", async (req: any, res: any) => {
+  const language = req.params.language as string;
   const snapshot = await firestore.collection("posts").get();
 
   res
@@ -18,16 +18,35 @@ router.get("/summary", async (req: any, res: any) => {
         const titles = x.get("titles") as Translation[];
         const summaries = x.get("summaries") as Translation[];
 
-        return new PostSummary(
+        return new Post(
           x.id,
           getTranslation(language, titles),
           x.get("author.name"),
           getTranslation(language, summaries),
+          x.get("languages"),
           x.get("datetime").toDate()
         );
       })
     )
     .send();
+});
+
+router.get("/:id/:language", async (req: any, res: any) => {
+  const id = req.params.id as string;
+  const language = req.params.language as string;
+  const snapshot = await firestore.collection("posts").doc(id).get();
+  const titles = snapshot.get("titles") as Translation[];
+  const contents = snapshot.get("contents") as Translation[];
+  const post: Post = new Post(
+    id,
+    getTranslation(language, titles),
+    snapshot.get("author.name"),
+    getTranslation(language, contents),
+    snapshot.get("languages"),
+    snapshot.get("datetime").toDate()
+  );
+
+  res.status(200).json(post).send();
 });
 
 const getTranslation = (
